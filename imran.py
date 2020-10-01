550be4e3697a4d744098be566c920ef3c8796afe
Mehafooz Ali Khan <48849945+Mehafooz@users.noreply.github.com>
2020-10-01 07:57:05 +0530

    Update imran.py

--- imran.py
+++ imran.py
@@ -8,3 +8,33 @@ if (Sum == Number):
     print(" %d is a Perfect Number" %Number)
 else:
     print(" %d is not a Perfect Number" %Number)
+   
+
+#using recursion
+
+#include<stdio.h>
+int sumdiv(int num, int x);
+ 
+int main()
+{
+        int num;
+        printf("Enter a number :");
+        scanf("%d",&num);
+        if(sumdiv(num, num/2) == num)
+                printf("\nPerfect Number\n");
+        else
+                printf("\nNot Perfect Number\n");
+}
+ 
+sumdiv(int num, int x)
+{
+        if(x==1)
+                return 1;
+        if(num%x==0)/*if x is a proper divisor*/
+                return x + sumdiv(num,x-1);
+        else
+                return sumdiv(num,x-1);
+}
+
+
+    
